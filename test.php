c9acfbbf707b2650edd1ba184373f90bb73fc83d
Ankit Jain <ankitengr@yahoo.com>
2014-12-02 17:45:46 +0530

    Ankit Jain
    Start

---
+++ test.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+require_once 'autoload.php';
+
+// Fill CLIENT ID, CLIENT SECRET ID, REDIRECT URI from Google Developer Console
+ $client_id = '74729835998-pockbrm42cavk92s7h9as49ml8klhsgu.apps.googleusercontent.com';
+ $client_secret = '3b5aXtlgXoVrza4pFXbkCROV';
+ $redirect_uri = 'http://localhost/projects/gittest/googleapi/test.php';
+ $simple_api_key = 'AIzaSyCR8pnhBgIbpjeHJOByh4EEhsccms36fQs';
+ 
+//Create Client Request to access Google API
+$client = new Google_Client();
+$client->setApplicationName("PHP Google OAuth Login Example");
+$client->setClientId($client_id);
+$client->setClientSecret($client_secret);
+$client->setRedirectUri($redirect_uri);
+$client->setDeveloperKey($simple_api_key);
+$client->addScope("https://www.googleapis.com/auth/userinfo.email");
+
+//Send Client Request
+$objOAuthService = new Google_Service_Oauth2($client);
+
+//Logout
+if (isset($_REQUEST['logout'])) {
+  unset($_SESSION['access_token']);
+  $client->revokeToken();
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL)); //redirect user back to page
+}
+
+//Authenticate code from Google OAuth Flow
+//Add Access Token to Session
+if (isset($_GET['code'])) {
+  $client->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client->getAccessToken();
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
+
+//Set Access Token to make Request
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+}
+
+//Get User Data from Google Plus
+//If New, Insert to Database
+if ($client->getAccessToken()) {
+  $userData = $objOAuthService->userinfo->get();
+  if(!empty($userData)) {
+	  echo '<pre>'; print_r($userData); echo '</pre>';
+	/*  
+	$objDBController = new DBController();
+	$existing_member = $objDBController->getUserByOAuthId($userData->id);
+	if(empty($existing_member)) {
+		$objDBController->insertOAuthUser($userData);
+	}
+	*/
+  }
+  $_SESSION['access_token'] = $client->getAccessToken();
+} else {
+  $authUrl = $client->createAuthUrl();
+}
+
+?>
